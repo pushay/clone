3d69da1d9f42eeb9e60a66e83a53263a08e6b528
pushay <sobocinskapraca@gmail.com>
2021-02-04 17:42:50 +0100

    adds Facebook SDK login

--- src/Components/LoginSign/LogSign.jsx
+++ src/Components/LoginSign/LogSign.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ImageSlider from '../ImageSlider/ImageSlider';
-import {LogSignTexts} from './LogSignText';
 import TextBlock from '../TextBlock/TextBlock';
 import TextBlockTexts from '../TextBlock/TextBlockText';
 import Form from '../Form/Form';
 
 function LogSign(props){
+    
+    useEffect(() => {
+        subscribeToLogin();
+    }, [])
+
+    const subscribeToLogin = () => {
+        if (window.FB) {
+            window.FB.Event.subscribe('auth.login', (response) => {
+                console.log(response)
+            });
+        } else {
+            setTimeout(() => subscribeToLogin(), 1000)
+        }
+    }
 
     return(
-        <div className='logSign' style={{padding:props.inputs === 'loginInput' ? '11.5rem 2rem' : '9.5rem 2rem'}}>
+        <div className='logSign' style={{padding:props.inputs === 'loginInput' ? '11.5rem 2rem' : '5.5rem 2rem'}}>
             {props.imageSlider === 'login' ? 
                 <ImageSlider imageSlider='login'/>
             : null 
@@ -18,14 +31,17 @@ function LogSign(props){
                 <div className='logSign__log'>
                     <TextBlock text={TextBlockTexts.textBlock1} />
                     <Form inputs={props.inputs}/>
+                    <span className='logSign__divide'>OR</span>
+                    <div className="fb-login-button logSign__facebook" data-width="278" data-size="large" data-button-type="continue_with" 
+                    data-layout="default" data-auto-logout-link="true" data-use-continue-as="false">
+                    </div>
                     {props.question === 'login' ? 
-                    <p className='logSign__bellowParagraph'>{LogSignTexts.signup.belowText}</p>
+                    <p className='logSign__bellowParagraph'>By signing up, you agree to our Terms. Any data submited will not be used against you and will be collected only for educational purpose of this project.</p>
                         : null    
                     } 
                 </div>
                 <TextBlock text={props.textBlock}/>
             </div>
-            
         </div>
     )
 }
--- src/Components/LoginSign/LogSignText.jsx
+++ src/Components/LoginSign/LogSignText.jsx
@@ -12,23 +12,4 @@ const InputsText = {
         ]
 }
 
-const LogSignTexts = {
-    login:{
-        question : {
-            question: 'Have an account?',
-            link:'/',
-            linkText:'Log in'
-        }
-    },
-    signup : {
-        headingText:'Sign up to see photos and videos from fake people.',
-        question : {
-            question:"Don't have an account?",
-            link:'/SignUp',
-            linkText:'Sign up'
-        },
-        belowText:'By signing up, you agree to our Terms. Any data submited will not be used against you and will be collected only for educational purpose of this project.'
-    }
-}
-
-export {InputsText, LogSignTexts}
+export {InputsText}
